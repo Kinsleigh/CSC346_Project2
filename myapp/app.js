99e338f0528c659a2645ce0f0dc73056f079d9c8
Dan Flores <drflores2@email.arizona.edu>
2019-04-03 18:52:26 -0700

    including formidable package in application

--- myapp/app.js
+++ myapp/app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var formidable = require('formidable');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
