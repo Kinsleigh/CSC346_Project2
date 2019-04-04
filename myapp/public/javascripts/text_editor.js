0a696f0edd8dec97ef475a6f9a137fec098e3d60
Kinsleigh <kinsleighwong@email.arizona.edu>
2019-04-03 22:26:15 -0700

    Add files via upload

--- myapp/public/javascripts/text_editor.js
+++ myapp/public/javascripts/text_editor.js
@@ -16,14 +16,9 @@ function change_fontstyle(){
 }
 
 function change_font(){
-    var type = document.getElementById("type").value.toLowerCase();
+    var type = document.getElementById("font").value;
+    console.log(type);
     document.getElementById("textblock").style.fontFamily = type;
-}
-
-function change_color(){
-    var col = document.getElementById("color").value.toLowerCase();
-    document.getElementById("textblock").style.color = col;
-
 }
 
 function change_size(){
@@ -31,21 +26,45 @@ function change_size(){
     document.getElementById("textblock").style.fontSize = size + "pt";
 }
 
+function change_tab(){
+    var tab_size =  document.getElementById("tab").value;
+    document.getElementById("textblock").style.tabSize = tab_size;
+}
+
 function upload_file(){
     console.log("Jokes");
+}
+
+//I borrowed this code from StackOverflow so I could use tabs for writing programs
+function replace_tab() {
+    var textareas = document.getElementsByTagName('textarea');
+    var count = textareas.length;
+    for(var i=0;i<count;i++){
+        textareas[i].onkeydown = function(e){
+            if(e.keyCode==9 || e.which==9){
+                e.preventDefault();
+                var s = this.selectionStart;
+                this.value = this.value.substring(0,this.selectionStart) + "\t" + this.value.substring(this.selectionEnd);
+                this.selectionEnd = s+1; 
+            }
+        }
+    }
 }
 
 if(document.readyState !== "loading") {
     document.getElementById("style").addEventListener("change", change_fontstyle);
     document.getElementById("font").addEventListener("change", change_font);
-    document.getElementById("color").addEventListener("change", change_color);
     document.getElementById("size").addEventListener("change", change_size);
+    document.getElementById("tab").addEventListener("change", change_tab);
     document.getElementById("up_file").addEventListener("click", upload_file);
+    document.getElementById("textblock").addEventListener("keydown", replace_tab);
 } else {
     document.addEventListener("DOMContentLoaded", function() {document.getElementById("style").addEventListener("change", change_fontstyle);});
     document.addEventListener("DOMContentLoaded", function() {document.getElementById("font").addEventListener("change", change_font);});
-    document.addEventListener("DOMContentLoaded", function() {document.getElementById("color").addEventListener("change", change_color);});   
     document.addEventListener("DOMContentLoaded", function() {document.getElementById("size").addEventListener("change", change_size);});
+        document.addEventListener("DOMContentLoaded", function() {document.getElementById("tab").addEventListener("change", change_tab);});
     document.addEventListener("DOMContentLoaded", function() {document.getElementById("up_file").addEventListener("click", upload_file);});
+    document.addEventListener("DOMContentLoaded", function() {document.getElementById("textblock").addEventListener("keydown", replace_tab);});
+
 } 
 
